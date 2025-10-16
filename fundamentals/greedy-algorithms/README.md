a2fe13f44045968c99110af3e431ff559986512e
David Privat <rossavikdavid@gmail.com>
2025-10-17 01:51:51 +0200

    feat: add new greedy algorithm, Huffman Coding"
    closes #16. closes #14, closes #13

--- fundamentals/greedy-algorithms/README.md
+++ fundamentals/greedy-algorithms/README.md
@@ -44,6 +44,43 @@ Optimal substructure:
 - After choosing a_k, we can ignore all activites that starts before the endtime of a_k
 - The rest of the problem has an identical structure: choose the maximum amount activites from the ones remaining
 - Optimal substructure PROVEN
+<br>
 
+### Fractional (Continuos) Knapsack
+
+- Greedy choice: Always bring items after biggest value/weight ratio, until te knapsack is full (last item is taken partially if nescesarry)
+
+Proof method: Exchange argument
+
+Greedy-choice property:
+- x_i is the amount of each item i we take
+- Assume that an optimal solution O dont take the best ratio first
+- Switch out a part of the item with lower ratio with a part of that of higher ratio
+- This increases the total value, without going over the weight capacity
+- Therefore the optimal solution is always going to obtain as much as possible from the item with higest value/weight ratio
+
+Optimal substructure:
+- After the first item is chosen (or parrts of it), we are left with a problem og same type
+  - Maximum value for a new capacity, and the suproblem has the same structure
+
+Note: For 0/1 Knapsack, greed fails. Because it isnt necessarily the "local" best combination that leads to the global optimum.
+
+<br>
+
+### Huffman Coding
+
+- Greedy choice: Always merge the two symbols with the lowest frequency to a new node
+
+Proof method: Induction + Exchange argument
+
+Greedy-choice property:
+- Assyme that there is an optimal treestructure T that dont combine the two lowest frequencies first
+- Let a,b be the two with lowest frequency
+- We can switch place on the undertrees in T, so that a and b are siblings, without increasing total cost
+- There there is always an optimal tree where the two lowest frequencies are combined first
+
+Optimal substructure:
+- When merging a and b to a new node with frequency of them combined, we end up with a new problem with one less node  same structure
+- Therefore Huffman-solution builds optimal from bottom to top.
 
 
---
+++ fundamentals/greedy-algorithms/huffman_coding.py
@@ -0,0 +1,129 @@
+"""
+huffman_coding.py
+------------------
+Implementationof huffman Coding using a Greedy Algorithm in Python.
+
+Background:
+    - Huffman coding is a lossless data compression algorithm
+    - It assigns variable-length binary codes to input symbols
+      such that more frequent symbols get shorter codes
+    - The result is a prefix-free binary encoding tree (node code
+       is a prefix of another / doesnt start with same binary codes)
+
+Problem:
+    - given a set of characters and their frequencies, build an 
+      optimal binary prefix code that minimizes the total cost:
+    - Cost =  Σ (frequency(char) * length(code(char)))
+
+Goal:
+    - Minimize the average code length (bits per symbol)
+    - Encode frequent symbols using fewer bits
+
+Main Idea (Greedy Strategy):
+    1. Start with all symbols as separate nodes with their frequencies.
+    2. Repeatedly merge the two nodes with the lowest frequencies.
+        > Create a new internal node whose frequency is the sum of the two
+        > Attach the two as left and right children
+    3. Continue merging until only one node (the root) remains
+    4. Traverse the tree:
+        > Assign 0 to left edges and 1 to right edges to form the code
+
+Why Greed Works:
+    - Greedy-choice property: Combining the two least frequent symbols first
+      never prevents an optimal prefix code
+    - Optimal substructure: After merging two symbols, the remaining (reduced
+      alphabet) has the same optimal structure
+
+Time Complexity:
+    - Building the tree: O(n logn)
+    - Generating codes: O(n)
+    - Total: O(n logn)
+
+Space Complexity:
+    - O(n): For the heap (priority queue) and Huffman tree
+
+Author: David
+Date: 17-10-2025
+"""
+
+import heapq
+
+class Node:
+    def __init__(self, char, freq): #initialize
+        self.char = char
+        self.freq = freq
+        self.left = self.right = None
+    
+    def __lt__(self, other): # Less than
+        return self.freq < other.freq
+    
+
+def build_huffman_tree(char_freqs):
+    """
+    Builds the Huffman tree using a greedy algorithm
+
+    The process:
+        1. Create a lead node for each character and insert it into a min-heap
+        2. While more than one node remains:
+            - Extract the two nodes with smallest frequencies
+            - Create a new internal node with frequency = sum of the two
+            - Set the two extracted nodes as children
+            - insert the new node back into the heap
+        3. The remaining node is the root of the Huffman tree
+    """
+
+    # STEP 1: Create initial heap with all characters
+    heap = [Node(c, f) for c,f in char_freqs]
+    heapq.heapify(heap)     # Converts list into valid min-heap (O(n))
+
+    # STEP 2: Merge the two smallest nodes until only one node remains
+    while len(heap) > 1:
+        # Extract two least frequent nodes (O(log n) each)
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+
+        # Create new internal node with combines freuency
+        merged = Node(None, left.freq + right.freq)
+        merged.left = left
+        merged.right = right
+
+        #Push the merged node back into the heap (O(logn))
+        heapq.heappush(heap, merged)
+    
+    # STEP 3: The heap now contains a single element = root of Huffman tree
+    return heap[0]
+
+def generate_codes(node, current_code="", codes=None):
+    """
+    Traverse the Huffman tree and generate binary codes for each character
+
+    The traversal:
+        - Follow the left child -> append '0' to the code
+        - Follow the right child -> append '1' to the code
+        - When a leaf node (char != None) is reached, store the code
+    """
+
+    if codes is None:
+        codes = {}
+    
+    if node is None:
+        return codes
+
+    # Leaf node: store its character and code
+    if node.char is not None:
+        codes[node.char] = current_code
+        return codes
+    
+    # Recursive traversal
+    generate_codes(node.left, current_code + "0", codes)
+    generate_codes(node.right, current_code + "1", codes)
+    return codes
+
+
+
+
+if __name__ == "__main__":
+    char_freqs = [('a', 5), ('b', 9), ('c', 12), ('d', 13), ('e', 16), ('f', 45)]
+    root = build_huffman_tree(char_freqs)
+    codes = generate_codes(root)
+    print("Huffman Codes:", codes)
